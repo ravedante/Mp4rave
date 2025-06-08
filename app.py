221f5f5334623bc3e1ed885aadcfa93cca25abcf
ravedante <ravedante6@gmail.com>
2025-06-08 19:58:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask
+import threading
+from bot import start_bot
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "videos"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/video/<path:filename>')
-def serve_video(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+@app.route('/')
+def home():
+    return 'Bot rodando com sucesso!'
+
+if __name__ == '__main__':
+    threading.Thread(target=start_bot).start()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
